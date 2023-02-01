fd52f8f0e9bf0d970034571f0b271de8f75a243a
Anti4iter4cb <124167821+Anti4iter4cb@users.noreply.github.com>
2023-02-01 17:35:14 +0300

    Update RACE CLICKER AUTO FARM GUI.lua

--- RACE CLICKER AUTO FARM GUI.lua
+++ RACE CLICKER AUTO FARM GUI.lua
@@ -6,7 +6,7 @@ local b = w:CreateFolder("Auto Farm") -- Creates the folder(U will put here your
 
 b:Label("Press for Wins",{
     TextSize = 25; -- Self Explaining
-    TextColor = Color3.fromRGB(Random); -- Self Explaining
+    TextColor = Color3.fromRGB("Random"); -- Self Explaining
     BgColor = Color3.fromRGB(254, 180, 33); -- Self Explaining
     
 }) 
