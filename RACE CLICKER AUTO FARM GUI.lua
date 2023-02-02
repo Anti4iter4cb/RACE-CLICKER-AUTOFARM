22276404cb2ce581d5b98c7226bbc6c4964109ea
Anti4iter4cb <124167821+Anti4iter4cb@users.noreply.github.com>
2023-02-02 11:14:26 +0300

    Update RACE CLICKER AUTO FARM GUI.lua

--- RACE CLICKER AUTO FARM GUI.lua
+++ RACE CLICKER AUTO FARM GUI.lua
@@ -1,6 +1,6 @@
 local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wall%20v3')))()
 
-local w = library:CreateWindow("WhyHub-- Creates the window
+local w = library:CreateWindow("WhyHub") -- Creates the window
 
 local b = w:CreateFolder("Auto Farm") -- Creates the folder(U will put here your buttons,etc)
 
